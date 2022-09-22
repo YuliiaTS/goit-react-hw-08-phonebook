ef3256ca8c2176b3f07d49f8492ab6c004c2fbb3
YuliiaTS <92184749+YuliiaTS@users.noreply.github.com>
2022-09-22 20:40:18 +0000

    Deploying to gh-pages from @ YuliiaTS/goit-react-hw-08-phonebook@efb980911ad90adf7a62a69e6082514584c5cc31 🚀

---
+++ static/js/Home.e097193d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[268],{433:function(e,r,t){t.r(r),t.d(r,{default:function(){return c}});var o="Home_homeWrap__rdn7v",n="Home_homeTitle__tmNjr",a=t(3329);function c(){return(0,a.jsx)("div",{className:o,children:(0,a.jsx)("h2",{className:n,children:"Phonebook: At Your Service"})})}}}]);
+//# sourceMappingURL=Home.e097193d.chunk.js.map
